8ffd7d0c225dfcf33922e97688289a969de3eaaf
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-02 15:35:06 +0100

    refactor grouped some imports

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import { Navbar } from "../components/Navbar";
-
 import { Container } from "@mui/system";
+import {TextField } from "@mui/material";
 
 export default function Home() {
 	return (
 		<Container maxWidth="xs">
-      
-    </Container>
+			<TextField 
+			color="text"
+			id="filled-search"
+			label='Search for a country...'
+			type="search"
+			sx={{maxWidth: 480, marginTop: 3, minWidth: '100%', backgroundColor: 'hsl(0, 0%, 100%)', }}
+			/>
+		</Container>
 	);
 }
