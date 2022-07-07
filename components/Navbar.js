66aab14cdcb840226140d943046dbd6a597f4697
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-07 19:04:52 +0100

    fix: updated navbar size

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,13 +5,13 @@ import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 export function Navbar() {
     return (
         <AppBar position="sticky">
-            <Toolbar>
+            <Toolbar  sx={{minHeight: '76px'}}>
                 <Container maxWidth='lg' sx={{display: 'flex', justifyContent: 'space-between'}}>
                     <Typography variant="h5">Where in the world?</Typography>
                     <div style={{display: 'flex'}}>
                         <DarkModeOutlinedIcon 
                         fontSize="small"
-                        sx={{mr: 0.250, pb: 0.4}}
+                        sx={{mr: 0.350, pb: 0.3}}
                         />
                         <Typography variant='body1' sx={{fontSize: 12, fontWeight: 600}}>Dark Mode</Typography>
                     </div>
