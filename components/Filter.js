404eba436de2b0e0a7797e42020fc71b7ad6cbfc
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-10 10:30:25 +0100

    feat!: now displays unique list of regions to filter

--- components/Filter.js
+++ components/Filter.js
@@ -1,20 +1,43 @@
-import { Autocomplete, createFilterOptions, TextField } from "@mui/material";
+import { useState } from "react";
 
-const filterOptions = createFilterOptions({
-    matchFrom: 'start',
-    stringify: country => country.region,
-})
+import { Box } from "@mui/system";
+import { Container } from "@mui/material";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 const Filter = ({countries}) => {
-    return ( 
-        <Autocomplete 
-			options={countries}
-			getOptionLabel={country => country.region}
-			sx={{minWidth: '53.33%', maxWidth: '200px'}}
-            filterOptions={filterOptions}
-			renderInput={params => <TextField  {...params} label='Filter by Region'/>}
-			/>
+    const [clickedOn, setClickedOn] = useState(false)
+
+    let regions = new Set(countries.map(country => country.region))
+    regions = Array.from(regions).sort();
+
+    return (
+        <Box>
+        <Box 
+        sx={{width: '60vw', maxWidth: '200px', height: '56px', 
+        backgroundColor: 'hsl(0, 0%, 100%)', borderRadius: '5px',
+        boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.04)', display: 'flex', justifyContent: 'space-between', alignItems:'center', padding: '18px 20px', '&:hover':{cursor: 'pointer'}}} 
+        component="span"
+        onClick={() => setClickedOn(!clickedOn)}> 
+            <Box component='span' sx={{fontWeight: '600', fontSize: '14px', WebkitUserSelect: 'none', userSelect: 'none'}}>Filter by Region</Box>
+            {!clickedOn && <ArrowDropDownIcon fontSize="medium"/> }
+            { clickedOn && <ArrowDropUpIcon fontSize="medium"/>}
+        </Box>
+
+        {clickedOn && <Box 
+            sx={{minHeight: '144px', width: '60vw', maxWidth: '200px', 
+            backgroundColor: 'hsl(0, 0%, 100%)', borderRadius: '5px',
+            boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.04)',  zIndex: 5, 
+            position: 'absolute', marginTop: '4px', WebkitUserSelect: 'none', userSelect: 'none'}}>
+                <Box component='ul' sx={{listStyle: 'none'}}>
+                    {regions.map(region => (
+                        <Box component='li' key={region} onClick={() => console.log(region)}
+                        sx={{marginBottom: '4px', fontWeight: '600', fontSize: 14, '&:hover':{fontWeight: 800, cursor: 'pointer'}}}>{region}</Box>
+                    ))}
+                </Box>
+            </Box>}
+        </Box>
      );
-}
- 
-export default Filter;
+    }
+    
+    export default Filter;
