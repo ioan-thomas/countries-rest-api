a84cde4e0ca2731521e67a360881d2bfba08d107
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-10 10:27:09 +0100

    feat: nav links to home, and hover change

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,28 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import Link from "next/link";
 
 export function Navbar() {
     return (
         <AppBar position="sticky">
             <Toolbar  sx={{minHeight: '76px'}}>
+
                 <Container maxWidth='lg' sx={{display: 'flex', justifyContent: 'space-between'}}>
-                    <Typography variant="h5">Where in the world?</Typography>
-                    <div style={{display: 'flex'}}>
+
+                    <Link href='/'>
+                        <Typography variant='h5' sx={{fontSize: [14, 14, 24], '&:hover':{cursor: 'pointer'}}}>Where in the world?</Typography>
+                    </Link>
+
+                    <Box component='div' sx={{display: 'flex', alignItems: 'center', '&:hover':{cursor: 'pointer'}}}>
                         <DarkModeOutlinedIcon 
                         fontSize="small"
                         sx={{mr: 0.350, pb: 0.3}}
                         />
                         <Typography variant='body1' sx={{fontSize: 12, fontWeight: 600}}>Dark Mode</Typography>
-                    </div>
+                    </Box>
                 </Container>
+
             </Toolbar>
         </AppBar>
     )
