6afdb0ec4f12cd24296a133e1f32e6666d46237a
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-11 11:56:49 +0100

    feat: created custom search component

--- components/SearchField.js
+++ components/SearchField.js
@@ -1,15 +1,36 @@
-import {TextField } from "@mui/material";
+import { Box } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
 
-const SearchField = () => {
+
+const SearchField = ({searchInput, changeSearchInput}) => {
+
+	const [something, setSomething] = useState('')
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setSomething(e.target.value)
+	}
+	
     return ( 
-        <TextField 
-			color="text"
-			id="filled-search"
-			label='Search for a country...'
-			type="search"
-			sx={{ width:'100%', maxWidth: '480px', backgroundColor: 'hsl(0, 0%, 100%)', marginRight: [0, 2], marginBottom: ['40px', '40px', 0]}}
-			/>
+		<Box sx={{ width:'100%', maxWidth: '480px', backgroundColor: 'hsl(0, 0%, 100%)', 
+		marginRight: [0, 2], marginBottom: ['40px', '40px', 0], padding: '19px 0 19px 24px', 
+		height: '56px', display: 'flex', alignItems: 'center', borderRadius: '5px',
+		boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.04)',}}>
+			<SearchIcon fontSize="medium" sx={{marginRight: '5%', color: 'hsl(0, 0%, 70%)'}}/>
+			<Box component='form'>
+				<Box
+					component="input"
+					placeholder='Search for a country...'
+					sx={{ width:'100%', border: 'none', '&::placeholder':{color: 'hsl(0, 0%, 50%)'}}}
+					onChange={ e => handleSubmit(e)}
+					value={something}
+					/>
+			</Box>
+		</Box>
      );
 }
  
 export default SearchField;
+
+// changeSearchInput(e.target.value)
