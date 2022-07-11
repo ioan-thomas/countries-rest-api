84d91f42024577f1ce07294f39771b0cf8c932fe
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-11 14:30:09 +0100

    feat: search functionality now working

--- components/CardLayout.js
+++ components/CardLayout.js
@@ -1,29 +1,40 @@
 import { Grid } from "@mui/material";
 import OneCard from "./Card";
 import { Container } from "@mui/system";
+import { useEffect, useState } from "react";
 
-const Cards = ({countries, filterRegion}) => {
+const Cards = ({countries, filterRegion, searchTerm}) => {
 
-    const countriesByRegion = countries.filter(country => {
-        switch (filterRegion) {
-            case 'all':
-                return true;
-            case 'Africa':
-            case 'Americas':
-            case 'Antarctic':
-            case 'Asia':
-            case 'Europe':
-            case 'Oceania':
-                return country.region === filterRegion
-                
-            default:
-                return true
-        }
-    })
+    console.log(countries[1].name.toLowerCase().includes(searchTerm));
+    let filteredCountries = countries;
+    
+    if (!searchTerm) {
+        filteredCountries = (countries.filter(country => {
+            switch (filterRegion) {
+                case 'all':
+                    return true;
+                case 'Africa':
+                case 'Americas':
+                case 'Antarctic':
+                case 'Asia':
+                case 'Europe':
+                case 'Oceania':
+                    return country.region === filterRegion
+                    
+                default:
+                    return true
+            }
+        }))
+    } else if (filterRegion === 'all'){
+        filteredCountries = (countries.filter(country => {
+            return country.name.toLowerCase().includes(searchTerm.toLowerCase())
+        }))
+    }
+
     return ( 
         <Container maxWidth="lg">
         <Grid container spacing={5} >
-            {countriesByRegion.map(country => (
+            {filteredCountries.map(country => (
                 <Grid item xs={12} sm={6} md={4} lg={3}key={country.name}>
                         <OneCard 
                         name={country.name || 'none listed'}
