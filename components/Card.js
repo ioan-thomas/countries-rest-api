6a6b26c5de4e7626199d8237efce7fe5de5507e7
Ioan Thomas <ioanthomas21@gmail.com>
2022-07-14 09:38:34 +0100

    feat: context now accessible and dark mode enabled

--- components/Card.js
+++ components/Card.js
@@ -7,17 +7,18 @@ import Link from 'next/link';
 
 
 const OneCard = ({name, population, region, capital, flags}) => {
+  
   const theme = useTheme()
     return (  
-      <Link href={`/${name.toLowerCase()}`}>
+      <Link href={"/[slug]"} as={`/${name.toLowerCase()}`}>
         <Card 
         sx={{ maxWidth: 345, minHeight: '336px', 
-        transition: 'all 0.3s ease', '&:hover':{transform: 'rotate(3deg)', cursor: 'pointer', backgroundColor: theme.palette.secondary}, backgroundColor: theme.palette.primary}}>
+        transition: 'all 0.3s ease', '&:hover':{transform: 'rotate(3deg)', cursor: 'pointer', backgroundColor: 'hoverColor.primary'}, backgroundColor: 'primary.main'}}>
             <Image
               component="img"
               height="160" 
               width='264'
-              src={flags.svg || flags.png}
+              src={flags.png || flags.svg}
               alt={name + ' flag'}
               layout="responsive"
               objectFit='cover'
